9bf2229475526d8bb0dd60a6ff984fb1daceb8d3
George Kettleborough (TGAC) <george.kettleborough@tgac.ac.uk>
2017-02-09 10:12:16 +0000

    Fix legend

--- gene-bars.R
+++ gene-bars.R
@@ -2,6 +2,7 @@
 
 library("ggplot2")
 library("reshape2")
+library("grid")
 
 t <- read.table("../genes-table", header=TRUE)
 tm <- melt(t, id.vars="asm", variable.name="type", value.name="genes")
@@ -15,7 +16,12 @@ p <- ggplot(tm, aes(x=asm, y=genes, fill=type)) +
                           "buscom"="Busco complete",
                           "buspar"="Busco partial",
                           "busmis"="Busco missing")) +
-    geom_bar(stat="identity", position="dodge", colour="grey")
+    geom_bar(stat="identity", position="dodge") +
+    geom_bar(stat="identity", position="dodge", colour="black", show_guide=FALSE) +
+    theme(legend.position="bottom", legend.key.size=unit(3, "mm"),
+          legend.text=element_text(size=8),legend.title=element_text(size=8),
+          plot.margin=unit(c(2,2,0,0), "mm")) +
+    guides(fill=guide_legend(nrow=2,byrow=TRUE))
 
 pdf(file="genes-bar.pdf", width=4, height=4)
 print(p)
