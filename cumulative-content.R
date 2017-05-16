44a0cf60c40c979a6723dea6dd80e2997fb663a7
George Kettleborough <george.kettleborough@earlham.ac.uk>
2017-05-16 13:36:08 +0100

    Fix cumulative content plot titles

--- cumulative-content.R
+++ cumulative-content.R
@@ -2,7 +2,9 @@
 
 library("ggplot2")
 library("grid")
+library("gridExtra")
 library("scales")
+library("plyr")
 
 reverselog_trans <- function(base = exp(1)) {
     trans <- function(x) -log(x, base)
@@ -16,17 +18,19 @@ input <- read.table("output/all-assemblies.minlen-cumulative",
                     sep=",",
                     col.names=c("minlen", "cumulative", "fname", "name"))
 
+input$fname <- revalue(input$fname, c("discovar-contig"="discovar"))
+
 ## "bigcomp" with the large assemblies
 bigcomp <- subset(input,
                   fname %in% c("10x-bn", "falcon-dt-bn",
                                "discovar-mp-dt-bn"))
-bigcomp$name <- factor(bigcomp$name,
-                       levels=c("Supernova + BioNano",
-                           "Falcon + DT + BioNano",
-                           "Discovar + MP + DT + BioNano"))
+bigcomp$fname <- factor(bigcomp$fname,
+                        levels=c("10x-bn",
+                                 "falcon-dt-bn",
+                                 "discovar-mp-dt-bn"))
 
 bigcompplot <-
-    ggplot(bigcomp, aes(x=minlen, y=cumulative, colour=name)) +
+    ggplot(bigcomp, aes(x=minlen, y=cumulative, colour=fname)) +
     scale_x_continuous(trans=reverselog_trans(10),
                        breaks=c(10000000,1000000,100000,10000,1000),
                        labels=c("10Mbp", "1MBp", "100kbp", "10kbp", "1kbp")) +
@@ -35,20 +39,20 @@ bigcompplot <-
                   labels=c("800Mbp", "100Mbp", "10Mbp", "1Mbp")) +
     xlab("Minimal scaffold length") +
     ylab("Covered assembly length\n") +
-    ggtitle("Comparison of potato assemblies") +
+    ggtitle(expression(paste("Comparison of ", italic("S. verrucosum"), " assemblies"))) +
     labs(colour="Assembly") +
     geom_line() +
     theme(legend.justification=c(1,0),legend.position=c(1,0))
 
 ## discovar comp
 dvcomp <- subset(input,
-                 fname %in% c("discovar-contig", "discovar-mp",
+                 fname %in% c("discovar", "discovar-mp",
                               "discovar-mp-bn", "discovar-mp-dt",
-                              "discovar-mp-dt-jelly",
+                              ## "discovar-mp-dt-jelly",
                               "discovar-mp-dt-bn"))
 
 dvcompplot <-
-    ggplot(dvcomp, aes(x=minlen, y=cumulative, colour=name)) +
+    ggplot(dvcomp, aes(x=minlen, y=cumulative, colour=fname)) +
     scale_x_continuous(trans=reverselog_trans(10),
                        breaks=c(10000000,1000000,100000,10000,1000),
                        labels=c("10Mbp", "1MBp", "100kbp", "10kbp", "1kbp")) +
@@ -57,7 +61,7 @@ dvcompplot <-
                   labels=NULL) +
     xlab("Minimal scaffold length") +
     ylab(NULL) +
-    ggtitle("Comparison of potato Discovar assemblies") +
+    ggtitle(expression(paste("Comparison of ", italic("S. verrucosum"), " Discovar assemblies"))) +
     labs(colour="Assembly") +
     geom_line() +
     theme(legend.justification=c(1,0),legend.position=c(1,0))
@@ -74,7 +78,7 @@ pbcomp <- subset(input,
                               "falcon-bn", "falcon-dt-bn"))
 
 pbcompplot <-
-    ggplot(pbcomp, aes(x=minlen, y=cumulative, colour=name)) +
+    ggplot(pbcomp, aes(x=minlen, y=cumulative, colour=fname)) +
     scale_x_continuous(trans=reverselog_trans(10),
                        breaks=c(10000000,1000000,100000,10000,1000),
                        labels=c("10Mbp", "1MBp", "100kbp", "10kbp", "1kbp")) +
@@ -83,7 +87,7 @@ pbcompplot <-
                   labels=NULL) +
     xlab("Minimal scaffold length") +
     ylab(NULL) +
-    ggtitle("Comparison of potato Falcon assemblies") +
+    ggtitle(expression(paste("Comparison of ", italic("S. verrucosum"), " Falcon assemblies"))) +
     labs(colour="Assembly") +
     geom_line() +
     theme(legend.justification=c(1,0),legend.position=c(1,0))
@@ -93,3 +97,15 @@ pbcompplotwaxis <- pbcompplot +
                   breaks=c(800000000,100000000,10000000,1000000),
                   labels=c("800Mbp", "100Mbp", "10Mbp", "1Mbp")) +
     ylab("Covered assembly length\n")
+
+## pdf(file="cumulative-content.pdf", width=15, height=5)
+## grid.arrange(bigcompplot, dvcompplot, pbcompplot, ncol=3)
+## dev.off()
+
+bigcompplotg <- ggplotGrob(bigcompplot)
+dvcompplotg <- ggplotGrob(dvcompplot)
+pbcompplotg <- ggplotGrob(pbcompplot)
+
+pdf(file="cumulative-content.pdf", width=15, height=5)
+grid.draw(cbind(bigcompplotg, dvcompplotg, pbcompplotg, size="last"))
+dev.off()
